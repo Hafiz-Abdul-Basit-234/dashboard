ec06168e87f2bed7def267aacf673085b445d579
Hafiz Abdul Basit <basitattari000@gmail.com>
2026-09-08 21:22:12 +0500

    do

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import Navbar from "../../components/nav/Nav";
 import UserList from "../../components/userlist/UserList";
 import "./Users.css";
-
+import  { useState } from "react";
+import { FiX } from "react-icons/fi";
 const users = [
   {
     id: "01",
@@ -102,13 +103,14 @@ const users = [
 ];
 
 const Users = () => {
+    const [showForm, setShowForm] = useState(false);
   return (
     <>
       <Navbar />
 
       {/* Add New Button */}
       <div className="users-actions">
-        <button className="add-user-btn">
+        <button className="add-user-btn"   onClick={() => setShowForm(true)}>
           Add New +
         </button>
       </div>
@@ -168,6 +170,78 @@ const Users = () => {
         </div>
 
       </main>
+{showForm && (
+  <div className="modal-overlay">
+    <div className="modal">
+
+      {/* HEADER */}
+      <div className="modal-header">
+        <h2>Add New User</h2>
+        <button onClick={() => setShowForm(false)}>✕</button>
+      </div>
+
+      {/* FORM */}
+      <form className="modal-form">
+
+        <div className="form-group">
+          <label>Name</label>
+          <input type="text" placeholder="e.g. Jordan Lee" />
+        </div>
+
+        <div className="form-group">
+          <label>Email</label>
+          <input type="email" placeholder="e.g. jordan@company.com" />
+        </div>
+
+        <div className="form-group">
+          <label>Country</label>
+          <input type="text" placeholder="e.g. New Lisbon" />
+        </div>
+
+        <div className="form-row">
+          <div className="form-group">
+            <label>Friends</label>
+            <input type="number" placeholder="0" />
+          </div>
+
+          <div className="form-group">
+            <label>Followers</label>
+            <input type="number" placeholder="1" />
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label>Status</label>
+          <select>
+            <option>Active</option>
+            <option>Pending</option>
+            <option>Rejected</option>
+          </select>
+        </div>
+
+        <button className="submit-btn">Add User</button>
+
+      </form>
+      <div className="modal-footer">
+  <button
+    type="button"
+    className="cancel-btn"
+    onClick={() => setShowForm(false)}
+  >
+    Cancel
+  </button>
+
+  <button
+    type="button"
+    className="submit-btn"
+  >
+    Add User
+  </button>
+    <FiX />
+</div>
+    </div>
+  </div>
+)}
     </>
   );
 };
